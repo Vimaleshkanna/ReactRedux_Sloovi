d2774f6f2f025a0873aa4367fe858e36840b61e7
Vimaleshkanna <69513265+Vimaleshkanna@users.noreply.github.com>
2022-04-03 00:17:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useEffect } from "react";
+import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { getAccess } from "./actions/auth";
 import Dashboard from "./component/Dashboard";
 import setAuthToken from "./utils/setAuthToken";
+import PropTypes from "prop-types";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
-const App = () => {
+const App = ({ getAccess, auth }) => {
+  useEffect(() => {
+    getAccess();
+    //alert(auth.code);
+    if (auth.code === 200) {
+      localStorage.setItem("token", auth.results.token);
+      localStorage.setItem("company_id", auth.results.company_id);
+    }
+  }, []);
   return (
     <>
       <Router>
@@ -22,4 +33,14 @@ const App = () => {
   );
 };
 
-export default App;
+App.propTypes = {
+  getAccess: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { getAccess })(App);
+
